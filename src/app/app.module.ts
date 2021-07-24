7280c4cb64233e198014f8633c95ed37a31a742c
santiago cortes <jeissoncortes02@outlook.com>
2021-07-24 10:45:26 -0500

    metodo get esta completo en el servicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {UsuarioService} from "./usuario.service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [UsuarioService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/usuario.service.ts
+++ src/app/usuario.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import {environment} from "../environments/environment";
 import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Usuario} from "./usuario";
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +13,8 @@ export class UsuarioService {
   private apiPruebaBackend = environment.apiPruebaBackend;
 
   constructor(private http: HttpClient) { }
+
+  public listarUsuarios(): Observable<Usuario[]>{
+    return this.http.get<Usuario[]>(`${this.apiPruebaBackend}/usuarios`);
+  }
 }
