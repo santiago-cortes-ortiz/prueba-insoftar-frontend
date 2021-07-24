e641309aaa6c9cd7f68761a67509b04a4365886a
santiago cortes <jeissoncortes02@outlook.com>
2021-07-24 10:48:13 -0500

    crear metodo para mostrar form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Usuario} from "./usuario";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prueba-users';
+
+  public mostrarFormulario(usuario: Usuario, modo: string): void{
+
+  }
+
 }
